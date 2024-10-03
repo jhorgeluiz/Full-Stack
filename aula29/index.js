d25af867d69c1c62f4462f944c9e7b326a994ff3
Jorge Luiz <j87150712@gmail.com>
2024-10-02 22:53:19 -0300

    aula 29

--- aula29/index.js
+++ aula29/index.js
@@ -1,4 +1,28 @@
-const data = new Date('1987-04-21  00:00:00')
-const diaSemana = data.getDay()
-
+const data = new Date('1987-04-24 00:00:00');
+let diaSemana = data.getDay();
 console.log(diaSemana)
+let diaSemanaTexto;
+
+switch (diaSemana) {
+case 0:
+    diaSemanaTexto = 'domingo';
+    break;
+case 1:
+    diaSemanaTexto = 'segunda';
+    break;
+case 2:
+    diaSemanaTexto = 'terça';
+    break;
+case 3:
+    diaSemanaTexto = 'quarta';
+    break;
+case 4:
+    diaSemanaTexto = 'quinta';
+    break;
+case 5:
+    diaSemanaTexto = 'sexta';
+    break;
+case 6:
+    diaSemanaTexto = 'sabado';
+    break;
+}
