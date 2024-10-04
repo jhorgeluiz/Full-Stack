c740a774a1888dcefd3783a0e92957506a919f82
Jorge Luiz <j87150712@gmail.com>
2024-10-03 22:35:39 -0300

    aula 30 exercico

---
+++ aula30/script.js
@@ -0,0 +1,36 @@
+const h1 = document.querySelector('.container h1')
+const data = new Date()
+
+
+function getDiaSemanaTexto(diaSemana) {
+ let diaSemanaTexto
+
+    switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'domingo';
+        return diaSemanaTexto;
+    case 1:
+        diaSemanaTexto = 'segunda-feira';
+        return diaSemanaTexto;
+    case 2:
+        diaSemanaTexto = 'terça-feira';
+        return diaSemanaTexto;
+    case 3:
+        diaSemanaTexto = 'quarta-feira';
+        return diaSemanaTexto;
+    case 4:
+        diaSemanaTexto = 'quinta-feira';
+        return diaSemanaTexto;
+    case 5:
+        diaSemanaTexto = 'sexta-feira';
+        return diaSemanaTexto;
+    case 6:
+        diaSemanaTexto = 'sabado';
+        return diaSemanaTexto;
+    default:
+        diaSemanaTexto = ''
+        return diaSemanaTexto
+    }
+}
+
+h1.innerHTML = getDiaSemanaTexto(data.getDay())
