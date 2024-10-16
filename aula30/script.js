aaefd320072f7ee9731f94d979eceef8dd0cd0a3
Jorge Luiz <j87150712@gmail.com>
2024-10-15 22:41:07 -0300

    aula

--- aula30/script.js
+++ aula30/script.js
@@ -104,4 +104,4 @@ const h1 = document.querySelector('.container h1')
 const data = new Date()
 
 
-h1.innerHTML = data.toLocaleDateString('pt-BR', {dateStyle: 'full'})
+h1.innerHTML = data.toLocaleDateString('pt-BR', {dateStyle: 'full '})
