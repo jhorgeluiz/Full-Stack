f88ff17c4cc73d6ae4e797c31fa81d1509f07768
Jorge Luiz <j87150712@gmail.com>
2024-10-17 21:51:45 -0300

    aula32

---
+++ aula32/index.js
@@ -0,0 +1,16 @@
+// atribuição dia desestruturação
+
+// ...spread
+
+            //    0   1   2   3   4   5   6   7   8  indice
+// const numeros = [100,200,300,400,500,600,700,800,900]
+// const [um, dois, tres, ...reto] = numeros;
+// console.log(um, dois, tres);
+// console.log(resto)
+
+
+                 //  0        1        2
+                // 0 1 2    3 4 5    6 7 8
+const numeros = [ [1,2,3], [4,5,6], [7,8,9] ]
+
+console.log(numeros[1][2])
