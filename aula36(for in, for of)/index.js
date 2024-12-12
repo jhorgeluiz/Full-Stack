649a4ce0bc4790036551af0649d1ad9fac81daf6
Jorge Luiz <j87150712@gmail.com>
2024-12-12 20:58:47 -0300

    aula36
    for, for in, for of, forEach

---
+++ aula36(for in, for of)/index.js
@@ -0,0 +1,24 @@
+// const frutas = ['pera', 'uva', 'maça']
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas)
+// }
+
+// for (let index in frutas) {
+//     console.log(frutas[index])
+// }
+
+
+
+// const nome = ['jorge', 'luiz', 'araujo']
+
+// for (let valor of nome) {
+//     console.log(valor)
+// }
+
+
+const nome = ['jorge', 'luiz', 'araujo']
+
+nome.forEach(function(valor, indice) {
+    console.log(valor, indice)
+})
