785bb14cc864cafe57dc20b1f7527ab5d0282f68
Jorge Luiz <j87150712@gmail.com>
2024-12-17 21:03:48 -0300

    aula41

---
+++ aula41/index.js
@@ -0,0 +1,25 @@
+// function max(x, y) {
+//     if (x > y) {
+//         return x
+//     } else {
+//         return y
+//     }
+// }
+// console.log(max(10, 2))
+
+
+// function max(x, y) {
+//     if (x > y) return x
+//     return y
+// }
+// console.log(max(10, 2))
+
+
+// function max(x, y) {
+//     return x > y ? x : y
+// }
+// console.log(max(10, 2))
+
+
+const max = (x, y) => x > y ? x : y
+console.log(max(10, 20))
