27414bcb07a00a1abe2cdd4566c0203fb35db8ee
Jorge Luiz <j87150712@gmail.com>
2024-12-20 21:17:34 -0300

    aula47

--- aula47/main.js
+++ aula47/main.js
@@ -1,4 +1,4 @@
-function segundos(segundos) {
+function criaHoraDosSegundos(segundos) {
     const data = new Date(segundos * 1000)
     return data.toLocaleTimeString('pt-br', {
         hour12:false, timeZone: 'UTC'
@@ -9,15 +9,30 @@ const relogio = document.querySelector('.relogio')
 const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
+let segundos = 0
+let timer
+
+function iniciaRelogio() {
+    timer = setInterval(function(){
+        segundos++
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
+}
 
 iniciar.addEventListener('click', function(event) {
-    alert('iniciou')
+    relogio.classList.remove('pausado')
+    clearInterval(timer)
+    iniciaRelogio()
 })
 
 pausar.addEventListener('click', function(event) {
-    alert('pausou')
+    relogio.classList.add('pausado')
+    clearInterval(timer)
 })
 
 zerar.addEventListener('click', function(event) {
-    alert('zerou')
+    relogio.classList.remove('pausado')
+    clearInterval(timer)
+    relogio.innerHTML = '00:00:00'
+    segundos = 0
 })
