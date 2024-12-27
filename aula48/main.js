76de20f6f927af3a94ee3928b2aa26b4277e4f5c
Jorge Luiz <j87150712@gmail.com>
2024-12-26 22:04:13 -0300

    Update main.js

--- aula48/main.js
+++ aula48/main.js
@@ -1,10 +1,33 @@
 
 const inputTarefa = document.querySelector('.input-tarefa')
 const btnAdd = document.querySelector('.btn-add')
-const tarefaarefa = document.querySelector('.tarefa')
+const tarefa = document.querySelector('.tarefa')
+
+function criaLi() {
+    const li = document.createElement('li')
+    return li
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return
+        criaTarefa(inputTarefa.value)
+    }
+})
+
+function limparInput() {
+    inputTarefa.value = ''
+    inputTarefa.focus()
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi()
+    li.innerHTML = textoInput
+    tarefa.appendChild(li)
+    limparInput()
+}
 
 btnAdd.addEventListener('click', function() {
     if (!inputTarefa.value) return
-
-    console.log(inputTarefa.value)
+    criaTarefa(inputTarefa.value)
 })
