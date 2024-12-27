ed53a75ff0c10f3b3547c6dda6a16efd90c9a9c7
Jorge Luiz <j87150712@gmail.com>
2024-12-27 20:44:26 -0300

    aula48

--- aula48/main.js
+++ aula48/main.js
@@ -20,14 +20,43 @@ function limparInput() {
     inputTarefa.focus()
 }
 
+function criaBotaoApagar(li) {
+    li.innerText += ''
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'apagar tarefa')
+    li.appendChild(botaoApagar)
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi()
     li.innerHTML = textoInput
     tarefa.appendChild(li)
     limparInput()
+    criaBotaoApagar(li)
+    salvarTarefa()
 }
 
 btnAdd.addEventListener('click', function() {
     if (!inputTarefa.value) return
     criaTarefa(inputTarefa.value)
 })
+
+document.addEventListener('click', function(e) {
+    const el = e.target
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove()
+    }
+})
+
+function salvarTarefa() {
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaDeTarefas = []
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('apagar', '').trim()
+        listaDeTarefas.push(tarefaTexto)
+    }
+}
